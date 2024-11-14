88fec7d3357f691a94b2ba56587adad0fb7052fd
souhailfoudhaili <souhail.foudhaili@esprit.tn>
2024-11-14 03:07:28 +0000

    add @id to the user.java

--- src/main/java/tn/esprit/devops_project/entities/User.java
+++ src/main/java/tn/esprit/devops_project/entities/User.java
@@ -2,7 +2,7 @@ package tn.esprit.devops_project.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-
+import javax.persistence.Id;
 @Entity
 // Optional, can be removed if not needed
 public class User {
