fe212e992d1db1e7f0fca5040801b376c0c38943
souhailfoudhaili <souhail.foudhaili@esprit.tn>
2024-11-14 03:21:41 +0000

    delete the user repo services  and the test for them

--- src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOps_ProjectSpringBootApplication.java
@@ -2,13 +2,10 @@ package tn.esprit.devops_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "tn.esprit.devops_project.repositories") // Specify your repository package
 public class DevOps_ProjectSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DevOps_ProjectSpringBootApplication.class, args);
     }
-}
