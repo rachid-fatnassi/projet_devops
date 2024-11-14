d6edd777c0c1b39dead6c41b77f6425f08e1b0ec
souhailfoudhaili <souhail.foudhaili@esprit.tn>
2024-11-14 00:03:36 +0000

    add test file

---
+++ src/test/java/tn/esprit/devops_project/ExampleTest.java
@@ -0,0 +1,13 @@
+// src/test/java/tn/esprit/devops_project/ExampleTest.java
+package tn.esprit.devops_project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExampleTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);  // A simple test that always passes
+    }
+}
+
