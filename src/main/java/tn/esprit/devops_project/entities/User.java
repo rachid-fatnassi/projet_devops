de71a17fc584a33792451e1c320c48d9fd46dcb3
souhailfoudhaili <souhail.foudhaili@esprit.tn>
2024-11-14 02:45:07 +0000

    modification in the main app.java ana the entities user

--- src/main/java/tn/esprit/devops_project/entities/User.java
+++ src/main/java/tn/esprit/devops_project/entities/User.java
@@ -1,5 +1,10 @@
 package tn.esprit.devops_project.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+// Optional, can be removed if not needed
 public class User {
     private String firstName;
     private String lastName;
